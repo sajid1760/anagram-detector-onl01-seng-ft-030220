3b30a63db0a71244b74e377e91ad11e4bb97383b
Sajid Anjum <sajid1760@gmail.com>
2020-03-15 04:35:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,18 @@
 class Anagram
   
+  @word = []
+  
   def initialize(word)
-    @word = word
+    @word = word.split("")
   end
   
   def match(matcharray)
+    retarray = []
     matcharray.each do |match|
+      if @word & match.split("") = @word then retarray << match end
+    end
+    retarray
+  end  
       
     
     
