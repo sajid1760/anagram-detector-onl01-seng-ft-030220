0311aa4c90675227d35d744d77d649cc7a682db5
Sajid Anjum <sajid1760@gmail.com>
2020-03-15 04:47:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+  
+  @word = []
+  
+  def initialize(word)
+    @word = word.split("")
+  end
+  
+  def match(matcharray)
+    retarray = []
+    matcharray.each do |match|
+      matchtest = match.split("")
+      if @word.sort == matchtest.sort
+        then retarray << match end
+    end
+    retarray
+  end  
+  
+end 
+      
+    
+  listen = Anagram.new("allergy")
+  puts listen.match(%w(gallery regally largely))
